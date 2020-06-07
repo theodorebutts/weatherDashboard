49ad1346aed504dc723a319a89daa76d81fa18c9
Theodore Butts <theodorebutts@gmail.com>
2020-06-06 19:19:52 -0600

    update initial framework

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+
+// fetch current day data from api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={your api key}
+
+// fetch 5 day data from api.openweathermap.org/data/2.5/forecast?q={city name},{state code},{country code}&appid={your api key}
+// display searched city current conditions
+// display 5 day forcast
+// display search history
